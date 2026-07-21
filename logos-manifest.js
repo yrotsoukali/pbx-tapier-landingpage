573dda7dcc173cba136817f74604c9823ab58f1f
yrotsoukali <yro.tsoukali@gmail.com>
2026-07-21 16:24:29 +0300

    updated social proof

--- logos-manifest.js
+++ logos-manifest.js
@@ -1,2 +1,2 @@
-﻿window.SOCIAL_PROOF_LOGOS = ["logos/i-metrix-wordmark-RGB-fullcolour-04.png","logos/logo.png","logos/logo-m-service-Blue-very-small-e1517068267722.jpg","logos/xit.png"];
+﻿window.SOCIAL_PROOF_LOGOS = ["logos/cropped-Analysis-Bottom-GooglePlusSmall-2.png","logos/i-metrix-wordmark-RGB-fullcolour-04.png","logos/loginet.png","logos/logo.png","logos/logo-m-service-Blue-very-small-e1517068267722.jpg","logos/xit.png"];
 
